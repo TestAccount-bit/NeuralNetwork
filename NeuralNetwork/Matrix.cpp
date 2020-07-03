e28bf74d0578a58762d4ae2b1e91d7d93b9593b1
TestAccount-bit <gerhugwsif@gmail.com>
2020-07-03 22:58:19 +0200

    Subscript operator() for matrix

--- NeuralNetwork/Matrix.cpp
+++ NeuralNetwork/Matrix.cpp
@@ -6,31 +6,77 @@
 
 using namespace std;
 
+//Matrix::Matrix(size_t rows, size_t cols)
+//{
+//	try {
+//		if (rows < 1) { throw 1; }
+//		if (cols < 1) { throw 2; }
+//		this->rows = rows;
+//		this->cols = cols;
+//		data = vector<double>(rows*cols, 0.0);
+//	}
+//	catch (int n) 
+//	{
+//		cout << "\"rows\" and \"cols\" must be more than 0" << endl;
+//		exit(0);
+//	}
+//	catch (exception& e) {
+//		cout << "exception in Matrix(size_t rows, size_t cols) constructor" << endl;
+//		exit(0);
+//		//abort();
+//	}
+//}
+
 Matrix::Matrix(size_t rows, size_t cols)
 {
-	try {
-		if (rows < 1) { throw 1; }
-		if (cols < 1) { throw 2; }
+	//cout << "rows: " << rows << "columns: " << cols << endl;
+	if (rows == 0 || cols == 1 || rows > 30 || cols > 30) 
+	{ 
+		cout << "Nonpositive amount of rows or/and columns" << endl;
+		exit(0);
+	}
+	else {
 		this->rows = rows;
 		this->cols = cols;
 		data = vector<double>(rows*cols, 0.0);
-	}
-	catch (exception& e) {
-		cout << "\"rows\" and \"cols\" must be more than 0" << endl;
-		abort();
 	}
 }
 
 Matrix::Matrix(size_t rows, size_t cols, std::vector<double> data)
 {
-	try {
-		this->rows = rows;
-		this->cols = cols;
-		this->data = data;
+
+	if (data.size() != rows * cols) {
+		cout << "rows*cols != data.size()" << endl;
+		exit(0);
 	}
-	catch (exception& e) {
+	if (rows == 0 || cols == 1 || rows > 30 || cols > 30) {
 		cout << "\"rows\" and \"cols\" must be more than 0" << endl;
-		abort();
+		exit(0);
+	}
+	this->rows = rows;
+	this->cols = cols;
+	this->data = data;
+}
+
+double & Matrix::operator()(size_t i=0, size_t j=0)
+{
+	if (i >= 0 && i < rows && j >= 0 && j < rows) {
+		return data.at(i*cols + j);
+	}
+	else {
+		cout << "Out of bounds " << endl;
+		exit(0);
+	}
+}
+
+double Matrix::operator()(size_t i, size_t j) const
+{
+	if (i >= 0 && i < rows && j >= 0 && j < rows) {
+		return data.at(i*cols + j);
+	}
+	else {
+		cout << "Out of bounds " << endl;
+		exit(0);
 	}
 }
 
@@ -39,12 +85,12 @@ Matrix::~Matrix()
 {
 }
 
-void Matrix::print()
+void Matrix::print(int precision)
 {
 	try {
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				cout << setw(3) << data.at(i*cols + j) << " ";
+		for (int i = 0; i < (int)rows; i++) {
+			for (int j = 0; j < (int)cols; j++) {
+				cout << setw(precision) << data.at(i*cols + j) << " ";
 			}
 			cout << endl;
 		}
--- NeuralNetwork/Matrix.h
+++ NeuralNetwork/Matrix.h
@@ -6,8 +6,11 @@ class Matrix
 public:
 	Matrix(size_t rows, size_t cols);
 	Matrix(size_t rows, size_t cols, std::vector<double> data);
+	double& operator()(size_t i, size_t j);
+	double operator()(size_t i, size_t j) const;
+
 	~Matrix();
-	void print();
+	void print(int precision=3);
 	std::vector<double> data;
 private:
 	size_t rows;
